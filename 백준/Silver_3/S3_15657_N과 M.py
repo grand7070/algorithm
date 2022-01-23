6e97be0183624274d5405f71c3373e0520d963a4
grand7070 <grand7070@naver.com>
2022-01-24 00:04:23 +0900

    4일차

--- 백준/Silver_3/S3_15657_N과 M.py
+++
@@ -1,7 +0,0 @@
-import sys
-from itertools import combinations_with_replacement
-n, m = map(int, sys.stdin.readline().split())
-tmp = sorted(list(map(int, sys.stdin.readline().split())))
-results = list(combinations_with_replacement((tmp), m))
-for result in results:
-	print(*result)
